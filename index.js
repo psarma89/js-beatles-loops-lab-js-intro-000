be7cc77633d628772d83e8e3a8f60310d498b567
Priyam Sarma <priyam.sarma@gmail.com>
2017-09-21 19:55:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians,instruments){
   bleh = [];
   for (var i = 0; i < musicians.length; i++) {
-    newArray.push(musicians[i] + " plays " + instruments[i])
+    bleh.push(musicians[i] + " plays " + instruments[i])
   }
   return bleh
 }
